1cc5fab99e92d054f9046e1ad7fc89269e06780d
lesliem <lesliem@LAT3520-15>
2023-06-07 23:29:48 +1000

    Attempted to get exchange rate function working

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let obj;
-
 const rateReq = new XMLHttpRequest();
-rateReq.addEventListener("load", function () { 
+rateReq.addEventListener("load", function () {
     let obj = JSON.parse(this.responseText)
-    //console.log(obj.data.AUD);
+    let usd = document.getElementById("USD").value;
+    document.getElementById("USD").addEventListener("input", calc);
+
+    function calc() {
+        document.getElementById("AUD").value = usd * parseInt(obj.data.AUD);
+    }
 });
-
-console.log(obj.data.AUD);
-
 rateReq.open("GET", "https://api.freecurrencyapi.com/v1/latest");
 rateReq.setRequestHeader("apikey", "llnkNFFEqohgupQnJFTO5oOq1DZiRxyvNdQGJHcR");
 rateReq.send();
+
