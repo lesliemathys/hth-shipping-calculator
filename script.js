7578d90d64e27cb7038261523bd6e534901f09a6
lesliem <lesliem@LAT3520-15>
2023-06-07 23:40:29 +1000

    Exchange function working

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ rateReq.addEventListener("load", function () {
     document.getElementById("USD").addEventListener("input", calc);
 
     function calc() {
-        document.getElementById("AUD").value = usd * parseInt(obj.data.AUD);
+        document.getElementById("AUD").value = document.getElementById("USD").value * parseFloat(obj.data.AUD);
     }
 });
 rateReq.open("GET", "https://api.freecurrencyapi.com/v1/latest");
