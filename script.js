a1a67c6e98d08fe60aaf0cf380dfc0b12a174ea5
lesliem <lesliem@LAT3520-15>
2023-06-07 23:59:37 +1000

    Shipping price per item logic

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const rateReq = new XMLHttpRequest();
 rateReq.addEventListener("load", function () {
     let obj = JSON.parse(this.responseText)
-    let usd = document.getElementById("USD").value;
-    document.getElementById("USD").addEventListener("input", calc);
 
-    function calc() {
+    document.getElementById("USD").addEventListener("input", purchasePricecalc);
+
+    let containerPrice = document.getElementById("container-price").addEventListener("input", shippingPriceCalc);
+    let containterQty = document.getElementById("container-qty").addEventListener("input", shippingPriceCalc);
+    let itemQty = document.getElementById("item-qty").addEventListener("input", shippingPriceCalc);
+
+
+    function purchasePricecalc() {
         document.getElementById("AUD").value = document.getElementById("USD").value * parseFloat(obj.data.AUD);
+        document.getElementById("purchase-price").value = document.getElementById("USD").value * parseFloat(obj.data.AUD);
+    }
+
+    function shippingPriceCalc() {
+        if (containerPrice > 0 && containterQty > 0 && itemQty > 0) {
+            document.getElementById("shipping-price").value = (containerPrice * containterQty) / itemQty;
+        }
     }
 });
 rateReq.open("GET", "https://api.freecurrencyapi.com/v1/latest");
